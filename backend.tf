13642c9762262fcb3070f58ab39abdb42d813bb3
Sai-Adhinatha-Reddy <subbugona@gmail.com>
2025-06-09 09:26:36 -0500

    Updated the bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-adhinatha-s3"  # The S3 bucket name
+    bucket         = "terraform-s3"  # The S3 bucket name
     key            = "terraform/state.tfstate" # Path to the state file inside the bucket
     region         = "us-east-1"               # Must match your S3 bucket's region
     encrypt        = true                      # Encrypt the state file at rest
